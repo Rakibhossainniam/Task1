78935283b26281b9178f40588e52a34775df4c99
Rakib hossain <31241583+Rakibhossainniam@users.noreply.github.com>
2017-08-23 23:45:11 -0700

    Odd and even number finding in C

---
+++ odd.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+ int i;
+printf("the even number between 1 to 50:");
+for(i=2;i<=100;i=i+2)
+printf("\n%d",i);
+
+
+{
+
+
+printf("the odd number between 1 to 50:");
+for(i=1;i<=100;i=i+2)
+printf("\n%d",i);
+}
+
+
+
+
+return 0;
+
+}
