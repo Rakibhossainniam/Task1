29c3d37c96cca614ff30d67b6726fc7b67923594
Rakib hossain <31241583+Rakibhossainniam@users.noreply.github.com>
2017-08-24 06:51:44 -0700

    yes

---
+++ boostrap/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>this is title</title>
+        <meta charset="utf-8">
+        <link href="css/css/bootstrap.css"rel="stylesheet"type="text/css"/>
+        <link href="css/css/bootstrap-responsive.css"rel="stylesheet"type="text/css"/>
+        <link href="style.css"rel="stylesheet"type="text/css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+    </head>
+    <body>
+   <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">DIVINE IT LTD.</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class=""><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+            <form class="navbar-form pull-right">
+              <input class="span2" type="text" placeholder="Email">
+              <input class="span2" type="password" placeholder="Password">
+              <button type="submit" class="	btn btn-success">log in</button>
+            </form>
+          </div>
+        </div>
+      </div>
+        </div>
+        <!--Slider -->
+    <div id="myCarousel" class="carousel slide">
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="css/img/divine1.png" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Welcome to Divine IT Limited</h1>
+              <p class="lead">Divine IT Limited is an IT consulting and software development company concentrating in customized ERP application and web-based solutions provider.</p>
+              <a class="btn btn-large btn-primary" href="#">Sign up today</a>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+           <img src="css/img/divine6.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Divine IT Limited, Dhaka, Bangladesh.</h1>
+              <p class="lead">Divine IT Limited is leading IT advancement contributor in Bangladesh specialized in ERP, EAM, SCM and Unified Messaging Solution.</p>
+              <a class="btn btn-large btn-primary" href="#">Learn more</a>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+           <img src="css/img/divine2.png" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>DIVINE IT SOLUTION.</h1>
+              <p class="lead">Divine IT Solutions delivers your web dream to a reality ranging from custom website design to development of complex enterprise web applications</p>
+              <a class="btn btn-large btn-primary" href="#">Browse gallery</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
+    </div>
+        
+        <!--main content-->
+        
+        <div class="main_content">
+            <div class="row">
+        <div class="span3">
+            <div class="img_1">
+          <a href=""><img class="img-rounded"src="css/img/download.jpg"/></a>
+            </div>
+          <h2><a href="">PrismERP</a></a></h2>
+          <p>Enterprise application enables organizations to measurable business suppliers and clients better user experiences. Divine IT Limited includes ERP, HR & Payroll, Accounting, Warehouse Management, LC Management.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div><!-- /.span3 -->
+        <div class="span3">
+           <a href=""><img class="img_1" src="css/img/linespay_banner.png"></a>
+          <h2><a href="">lines Pay</a></h2>
+          <p>LinesPay is a web based, comprehensive, and flexible human resources and payroll management software.The solution influences the management and decision maker for better strategic business policies and will provide the finest control over employees & payroll management. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div><!-- /.span3 -->
+        <div class="span3">
+          <a href=""><img class="img_1" src="css/img/sphere_banner.png"></a>
+          <h2><a href="">Sphere Accounting</a></a></h2>
+          <p>Sphere Accounting makes organizational finance on a solid ground and helps companies streamline enterprise-wide processes, achieve economies of scale, and improves visibility into performance. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+          <div class="span3">
+           <a href=""><img class="img_1" src="css/img/centroid_banner.png"></a>
+          <h2><a href="">Centroid LC</a></h2>
+          <p>Tracking documentation and financial information efficiently and accurately of LCs are the vital parts of a business success for all organization. Centroid, a LC management system keeps track of all import and export LCs’ information effortlessly and accurately. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>       <!-- /.span4 -->
+      </div>
+        
+        </div>
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>DIVINE IT LTD &copy; 2017.    &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+            <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
+    <script>
+      !function ($) {
+        $(function(){
+          // carousel demo
+          $('#myCarousel').carousel()
+        })
+      }(window.jQuery)
+    </script>
+    <script src="../assets/js/holder/holder.js"></script>
+    </body>
+</html>
